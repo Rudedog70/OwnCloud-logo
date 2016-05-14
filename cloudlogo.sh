c765fc0287fe0d7ab4bbbc013418504e9e8e950b
Rudedog70 <rudedog70@gmail.com>
2016-05-13 23:15:06 -0400

    Add files via upload

---
+++ cloudlogo.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo "\033[38;5;110m
+                                                                     ,########,
+                                                                   ,#############,
+                                                           ,####,,################,
+                                                         ,######,##################,
+                                                         ######,####################,####o
+                                                        ,###### ,########,######,###,######o
+                                                         #'oOOOOO,OOOO,OOOOOOOOOOOO,OOOOOOOOo
+                                                      ,oOOOOOOOOOO,o,OOOOOOOOOOOOOOOOO OOOOOo,
+                                                     ,oOOOOOOOOOOO,OOOOOOOOOOOOOOOOOOOOO OOo OOOO,
+                                                ,,OO OOOOOOOOOOOo,OOOOOOOOOOOOOOOOOOOOOOO oOOOOOOOOO,
+                                             ,OOOOOO oOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOO,
+                                            ,%%%%%%% %%%%%%%%%%%,%%%%%%%%%%%%%%%%%%%%%%%%%,%%%%%%%%%%%%%
+                                           ,%%%%%%%%% %%%%%%%%%%,%%%%%%%%%%%%%%%%%%%%%%%%%,%%%%%%%%%%%%%%
+                                           %%%%%%%%%%%% %%%%,%%% %%%%%%%%%%%%%%%%%%%%%%%%% %%%%%%%%%%%%%% %%,
+                                          ,%%%%%%%%%%%%%%% %%%%%,%%%%%%%%%%%%%%%%%%%%%%%%%,%%%%%%%%%%%%%%,%%%%,
+                                           %%%%%%%%%%%%%% %%%%%%% %%%%%%%%%%%%%%%%%%%%%%% %%%%%%%%%%%%%%% %%%%%%
+                                           ,%%%%%%%%%%%%%,%%%%%%%%,%%%%%%%%%%%%%%%%%%%%%,%%%%%%%%%%%%%%% %%%%%%%\033[38;5;63m       dd
+\033[38;5;110m                                              %%%%%%%%%%% %%%%%%%%%% %%%%%%%%%%%%%%%%  %%%%%%%%%%%%%%% %%%%%%%%\033[38;5;63m        dd
+\033[38;5;110m                                                %%%%%%%\033[38;5;63m   *cc\033[38;5;110m%%%%%%%\033[38;5;63m ll\033[38;5;110m o%%%%%%%%%o       %%%%%%%%%%    %%%%%\033[38;5;63m          dd
+                                                        *cc          ll                                                dd
+                                                      *cc            ll                                                dd
+                                                     *cc             ll                                          ......dd
+\033[38;5;166m    ,ooooooo,    *ww     ww     *ww   nnnnnnnnnnn  \033[38;5;63m  ccc             ll       ,ooooooo,     uu        *uu    ,ddddddddddd
+\033[38;5;166m   oo       oo   *ww     ww     *ww  ,nn       *n* \033[38;5;63m  ccc             ll      oo       oo    uu        *uu   dd         dd
+\033[38;5;166m  oo         oo  *ww     ww     *ww  nn         nn*\033[38;5;63m  ccc             ll     oo         oo   uu        *uu  *d*         dd
+\033[38;5;166m  oo         oo  *ww     ww     *ww  nn         nn*\033[38;5;63m   cc*            ll     oo         oo   uu        *uu  *d*         dd
+\033[38;5;166m   oo       oo   *ww    wwww    *ww  nn         nn*\033[38;5;63m    ccc*          ll,     oo       oo    uuu      u*uu   dd         dd
+\033[38;5;166m    ,ooooooo,     *WWwwwwwwwwwwWW*   nn         nn*\033[38;5;63m       ccc*        lll*    ,ooooooo,       uuuuuuuuu      ddddddddddd
+\033[38;5;166m      ''o''          ,ww    ,ww      nn         nn*\033[38;5;63m           cc        ll      ''o''           ''u''           'ddd''
+\033[0m\n"
